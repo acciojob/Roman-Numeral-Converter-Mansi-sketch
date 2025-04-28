5e44281ea15ded1c3b12825763ecda03852f987e
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-04-28 09:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function convertToRoman(num) {
   	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+      ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
     };
 
   //your code here
 
 	let roman = '';
-for (let i = 0; i <= 6; i++) {
+for (let i = 0; i <= 13; i++) {
 	while (num >= obj[i][1]) {
 		roman += obj[i][0];
 		num -= obj[i][1];
